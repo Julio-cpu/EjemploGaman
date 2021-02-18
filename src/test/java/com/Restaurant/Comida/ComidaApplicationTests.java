b92c4c6de727c7e7d420ccb42a30e627086376ab
Julio Cesar <juliocesarsalazar25@hotmail.com>
2021-02-18 12:19:39 -0700

    primer commit

---
+++ src/test/java/com/Restaurant/Comida/ComidaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Restaurant.Comida;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComidaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
